dec8be10a41f8952640a9b488bdae31f9980740c
DialloMaria <diallomariam0715@gmail.com>
2024-07-28 19:16:51 +0000

    MIse en place de la fonctionnalité qui nous permet de recupérer les contacts

--- src/app/contact.service.ts
+++ src/app/contact.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Contact } from './models/contact';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
 
+  private contactsKey = 'contacts';
   constructor() { }
+
+  getContacts(userId: string): Contact[] {
+    const contacts = JSON.parse(localStorage.getItem(this.contactsKey) || '[]');
+    return contacts.filter((contact: Contact) => contact.createdBy === userId);
+  }
+
 }
