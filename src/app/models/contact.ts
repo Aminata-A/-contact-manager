e56bd5f77c2189ad09536b366b982a9af31b4dd7
Aminata-A <aminataassane.ndiaye@gmail.com>
2024-07-28 15:54:09 +0000

    mise en place des models contact et user pour la mise en place de leur interface

---
+++ src/app/models/contact.ts
@@ -0,0 +1,14 @@
+export interface Contact {
+    id: string;
+    nom: string;
+    prenom: string;
+    email: string;
+    telephone: string;
+    createdAt: Date;
+    createdBy: string;
+    updatedAt?: Date;
+    updatedBy?: string;
+    description: string;
+    image?: string;
+  }
+  
