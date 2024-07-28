98e49e578329644ea5da42215ee96f07c9eb3cad
DialloMaria <diallomariam0715@gmail.com>
2024-07-28 22:01:48 +0000

    Creataion des Routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,24 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+// import { ApplicationConfig } from '@angular/core';
+// import { routes } from './app.routes';
+import { Route,provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { LoginComponent } from './login/login.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { EditContactComponent } from './edit-contact/edit-contact.component';
+import { TrashComponent } from './trash/trash.component';
 
-export const appConfig: ApplicationConfig = {
+const routes: Route[] = [
+  { path: 'contacts', component: ContactListComponent },
+  { path: 'add-contact', component: ContactFormComponent },
+  { path: 'edit-contact/:id', component: EditContactComponent },
+  { path: 'trash', component: TrashComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', redirectTo: '/login' }
+]
+
+
+export const appConfig = {
   providers: [provideRouter(routes), provideAnimationsAsync()]
 };
